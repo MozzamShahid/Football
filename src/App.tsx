ab32a50e0b011688d17e8e0a7c28cfb3c3f8be89
Mozzam Shahid <mozzam@Mozzams-MacBook-Air.local>
2024-05-23 13:55:26 +0500

    Initial Push

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Hello World!
+      </h1>
+      <p>My Name is Mozzam Shahid</p>
+    </>
+  )
+}
+
+export default App
